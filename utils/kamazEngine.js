c1f5c7b9bdf5c39c4d0551c0fc06bfd52601e2eb
tatonka393 <66238300+tatonka393@users.noreply.github.com>
2024-04-24 12:20:58 +0500

    заполнил класс

--- utils/kamazEngine.js
+++ utils/kamazEngine.js
@@ -1,17 +1,49 @@
 
-
+const gpio = require("@iiot2k/gpio");
+gpio.init_gpio(26, gpio.GPIO_MODE_OUTPUT, 0);
+gpio.init_gpio(20, gpio.GPIO_MODE_OUTPUT, 0);
+gpio.init_gpio(5,gpio.GPIO_MODE_OUTPUT,1) //кнопка
+//gpio.init_gpio(22,gpio.GPIO_MODE_INPUT_PULLDOWN,1000); 
+gpio.init_gpio(6,gpio.GPIO_MODE_INPUT_PULLDOWN,1000); //кнопка
+gpio.init_gpio(13,gpio.GPIO_MODE_INPUT_PULLDOWN,1000); //кнопка
 class KamazEngine{
     #direction
+    #onMove
 
     constructor(){
+        this.#onMove = false
         this.#direction = true;
+        setInterval(()=>{
+            if(this.#onMove){
+                if(gpio.get_gpio(6)){
+                    gpio.set_gpio(26,0)
+                    gpio.set_gpio(20,1)
+                }if(gpio.get_gpio(13)){
+                    gpio.set_gpio(26,1)
+                    gpio.set_gpio(20,0)
+                }
+                if(!gpio.get_gpio(13)&&!gpio.get_gpio(6)){
+                gpio.set_gpio(26,0)
+                gpio.set_gpio(20,0)
+                }
+            }
+        },30)
     }
     
     start(){
-
+        if(this.#direction){
+            gpio.set_gpio(26,0)
+            gpio.set_gpio(20,1) 
+        }else{
+            gpio.set_gpio(26,1)
+            gpio.set_gpio(20,0)
+        }
+        this.#onMove = true
     }
     stop(){
-
+        gpio.set_gpio(26,0)
+        gpio.set_gpio(20,0)
+        this.#onMove = false
     }
     /**
      * Изменяет направление в зависимости от указанного значения.
@@ -22,7 +54,7 @@ class KamazEngine{
         if(direction == 'up')
             this.#direction = true;
         if(direction == 'down')
-            this.direction = false;
+            this.#direction = false;
     }
     getDirection(){
         return this.#direction;
